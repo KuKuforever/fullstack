da8e04b0b6cbd3969a1c10a32c3a2f5771df6211
kukuforever <kanxue0012@gmail.com>
2018-05-16 10:24:21 -0400

    added controller implementation

--- ProjectForFun/src/main/java/com/basicspringproject/fullstackdemo/controllers/AccountController.java
+++ ProjectForFun/src/main/java/com/basicspringproject/fullstackdemo/controllers/AccountController.java
@@ -1,13 +1,31 @@
 package com.basicspringproject.fullstackdemo.controllers;
 
 
+import com.basicspringproject.fullstackdemo.domain.User;
+import com.basicspringproject.fullstackdemo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 @CrossOrigin(origins = "*")
 @RequestMapping("/account")
 public class AccountController {
+    UserService userService;
 
+    @Autowired
+    public AccountController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/all")
+    @ResponseBody
+    public List<User> getUsers() {
+        return userService.findAll();
+    }
 }
