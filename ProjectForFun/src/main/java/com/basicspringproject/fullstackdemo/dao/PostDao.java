a6d1c33f59ae53c3866a5c842d92325341120884
honghu215 <huhong215@gmail.com>
2018-05-31 11:35:55 -0400

    Added Post Service

--- ProjectForFun/src/main/java/com/basicspringproject/fullstackdemo/dao/PostDao.java
+++ ProjectForFun/src/main/java/com/basicspringproject/fullstackdemo/dao/PostDao.java
@@ -1,11 +1,12 @@
-//package com.basicspringproject.fullstackdemo.dao;
-//
-//import com.basicspringproject.fullstackdemo.domain.Post;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//import java.io.Serializable;
-//
-//@Repository
-//public interface PostDao extends JpaRepository<Post, Serializable> {
-//}
+package com.basicspringproject.fullstackdemo.dao;
+
+import com.basicspringproject.fullstackdemo.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.io.Serializable;
+
+@Repository
+public interface PostDao extends JpaRepository<Post, Serializable>{
+
+}
--- ProjectForFun/src/main/java/com/basicspringproject/fullstackdemo/domain/Post.java
+++ ProjectForFun/src/main/java/com/basicspringproject/fullstackdemo/domain/Post.java
@@ -1,4 +1,48 @@
 package com.basicspringproject.fullstackdemo.domain;
 
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="POST")
 public class Post {
+
+    private int id;
+    private String status;
+    private int imageId;
+    private String title;
+    private int posterId;
+
+    @Id
+    @Column(name="ID")
+    public int getId() { return id; }
+
+    public void setId(int id) { this.id = id; }
+
+    @Column
+    public String getStatus() { return status; }
+
+
+
+
+    public int getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getPosterId() {
+        return posterId;
+    }
+
+
 }
